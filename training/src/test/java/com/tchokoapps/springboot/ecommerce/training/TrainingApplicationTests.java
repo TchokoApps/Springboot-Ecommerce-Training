1dbd1644b74b512a63e6c75658131e68820d4df4
TchokoApps <tchokoapps@gmail.com>
2021-10-01 16:36:24 +0200

    compent scan removed

--- training/src/test/java/com/tchokoapps/springboot/ecommerce/training/TrainingApplicationTests.java
+++ training/src/test/java/com/tchokoapps/springboot/ecommerce/training/TrainingApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
-@ComponentScan({"com.tchokoapps.springboot.ecommerce.training.services"})
 class TrainingApplicationTests {
 
 	@Test
